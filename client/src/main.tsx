b1f42fa173f2c76051841ff67770d7a75dee1564
Ibrahim <tp078407@mail.apu.edu.my>
2025-11-15 01:21:33 +0800

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App.jsx"; // <-- This is the fix
 import "./index.css";
-// import "./styles.css";
+// We are importing styles from index.css now
 
 createRoot(document.getElementById("root")!).render(<App />);
