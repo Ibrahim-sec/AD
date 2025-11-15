10333f18b711a4ec5ec216f37b99d53208234218
Ibrahim <tp078407@mail.apu.edu.my>
2025-11-15 19:12:19 +0800

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx"; // <-- This is the fix
 import "./index.css";
-import { ThemeProvider } from 'next-themes';
-
 // We are importing styles from index.css now
 
-createRoot(document.getElementById("root")!).render(
-  <ThemeProvider defaultTheme="dark">
-    <App />
-  </ThemeProvider>
-);
+createRoot(document.getElementById("root")!).render(<App />);
+
