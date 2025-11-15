bfe9f3ac0f060c5dfe8cc4029bd0f0364c822955
Ibrahim <tp078407@mail.apu.edu.my>
2025-11-16 05:16:22 +0800

    Update index.js

--- client/src/data/diagrams/index.js
+++ client/src/data/diagrams/index.js
@@ -1,12 +1,22 @@
-// client/src/data/diagrams/index.js
-
 export { kerberoastingDiagram } from './kerberoastingDiagram';
 export { asrepRoastingDiagram } from './asrepRoastingDiagram';
 export { adTopologyDiagram } from './adTopologyDiagram';
 
-// Central registry for easy access
+export { ntlmRelayDiagram } from './ntlmRelayDiagram';
+export { gppPasswordsDiagram } from './gppPasswordsDiagram';
+export { zerologonDiagram } from './zerologonDiagram';
+export { printNightmareDiagram } from './printNightmareDiagram';
+export { skeletonKeyDiagram } from './skeletonKeyDiagram';
+
+// Central registry for easy async import access
 export const diagramRegistry = {
   kerberoasting: () => import('./kerberoastingDiagram').then(m => m.kerberoastingDiagram),
   asrepRoasting: () => import('./asrepRoastingDiagram').then(m => m.asrepRoastingDiagram),
   adTopology: () => import('./adTopologyDiagram').then(m => m.adTopologyDiagram),
+  
+  ntlmRelay: () => import('./ntlmRelayDiagram').then(m => m.ntlmRelayDiagram),
+  gppPasswords: () => import('./gppPasswordsDiagram').then(m => m.gppPasswordsDiagram),
+  zerologon: () => import('./zerologonDiagram').then(m => m.zerologonDiagram),
+  printNightmare: () => import('./printNightmareDiagram').then(m => m.printNightmareDiagram),
+  skeletonKey: () => import('./skeletonKeyDiagram').then(m => m.skeletonKeyDiagram),
 };
