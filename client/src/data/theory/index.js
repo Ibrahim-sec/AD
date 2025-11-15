641137c28490f1b737ea740d0b0aeb755304631f
Ibrahim <tp078407@mail.apu.edu.my>
2025-11-16 04:57:31 +0800

    Update index.js

--- client/src/data/theory/index.js
+++ client/src/data/theory/index.js
@@ -1,1 +1,46 @@
-index.js
+// client/src/data/theory/index.js
+
+import { kerberoastingTheory } from './kerberoasting';
+import { asrepRoastingTheory } from './asrepRoasting';
+import { dcsyncTheory } from './dcsync';
+import { passTheHashTheory } from './passTheHash';
+import { bloodhoundTheory } from './bloodhound';
+import { goldenTicketTheory } from './goldenTicket';
+
+// Main export: dictionary of all modules
+export const theoryModules = {
+  'kerberoasting': kerberoastingTheory,
+  'asrep-roasting': asrepRoastingTheory,
+  'dcsync': dcsyncTheory,
+  'pass-the-hash': passTheHashTheory,
+  'bloodhound': bloodhoundTheory,
+  'golden-ticket': goldenTicketTheory
+};
+
+/**
+ * Get theory module by scenario ID
+ */
+export const getTheoryModule = (scenarioId) => {
+  return theoryModules[scenarioId] || null;
+};
+
+/**
+ * Check if scenario has theory module
+ */
+export const hasTheoryModule = (scenarioId) => {
+  return scenarioId in theoryModules;
+};
+
+/**
+ * Get all theory modules
+ */
+export const getAllTheoryModules = () => {
+  return Object.values(theoryModules);
+};
+
+/**
+ * Get theory modules by difficulty
+ */
+export const getTheoryModulesByDifficulty = (difficulty) => {
+  return Object.values(theoryModules).filter(module => module.difficulty === difficulty);
+};
